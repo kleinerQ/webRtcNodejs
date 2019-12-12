e7acd89a38fad6fbbb8e3c21664606014ee58185
chihyen <chih.yen5408@gmail.com>
2019-12-12 20:14:11 +0800

    update ip

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const static = require('node-static');
 const http = require('http');
