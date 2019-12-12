dc7d87bfd15119cfebb3ef9adb6d6000a70c93cf
chihyen <chih.yen5408@gmail.com>
2019-12-12 19:14:31 +0800

    update static

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
 const static = require('node-static');
 const http = require('http');
 const file = new(static.Server)();
